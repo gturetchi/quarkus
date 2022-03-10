a8208cef6605fc0da3007da115e8ff22f5df38ad
gturetchi <84505976+gturetchi@users.noreply.github.com>
2022-03-10 15:02:06 +0200

    JsonObject added.

---
+++ src/main/java/org/test/JsonExample.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class JsonExample {
+
+    public String word;
+    public String UUID;
+    public String IP;
+
+    public JsonExample() {}
+
+    public JsonExample(String word, String UUID, String IP){
+        this.word = word;
+        this.UUID = UUID;
+        this.IP = IP;
+    }
+
+
+}
